eace9c7520a3f2248a66e20db472d05fb6c18dab
kumarcski <kumar07csk@gmail.com>
2021-03-18 10:09:52 +0530

    half completed

---
+++ src/com/org/practice/A.java
@@ -0,0 +1,33 @@
+package com.org.practice;
+
+public class A {
+
+
+
+		int a=3,b=10;
+		double f=0;
+		public void greensOmr() {
+		int c=a+b;
+	     System.out.println("REALTIME EXPERIENCE"+" "+c);
+		}
+	    public void greensAdayar() {
+	    	int d=a-b;
+	     System.out.println("MUCH BETTER"+" "+d);
+		}
+	    public void greensTambaram() {
+	    System.out.println("nice"+" "+f);
+		}
+	    public void greensValecherry() {
+	    System.out.println("worst");
+		}
+	    public static void main(String[] args) {
+			A g1=new A();
+			g1.greensOmr();
+			g1.greensAdayar();
+			g1.greensTambaram();
+			g1.greensValecherry();
+		}
+	}
+
+
+
